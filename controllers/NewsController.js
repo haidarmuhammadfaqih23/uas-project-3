5c6b75194051ee67e3e0cbc1d0f3c61436902fd5
haidarmuhammadfaqih23 <karyahaidar@gmail.com>
2024-01-19 10:02:22 +0700

    uas backend 3

---
+++ controllers/NewsController.js
@@ -0,0 +1,12 @@
+// import Model News
+
+// buat class NewsController
+class NewsController {
+  // buat fungsi
+}
+
+// membuat object NewsController
+const object = new NewsController();
+
+// export object NewsController
+module.exports = object;
